395571801ee45e9dc1cfbfc6cdf1ea25cdcc68f6
Ayaan Hafeez <m.ayaan.hafeez@gmail.com>
2026-08-15 00:26:09 +0500

    ssht 0.1.3

--- Formula/ssht.rb
+++ Formula/ssht.rb
@@ -1,8 +1,8 @@
 class Ssht < Formula
   desc "Smart SSH session manager that auto-attaches to persistent tmux sessions"
   homepage "https://github.com/mayaanhafeez/ssht"
-  url "https://github.com/mayaanhafeez/ssht/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "34ac1d5f0cb92f9463dda0e9f7037f569951cd57242eb76ec449e2282a9a6d77"
+  url "https://github.com/mayaanhafeez/ssht/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "3419c1019fb8e2bdfd73603d1219adaee043e5327fb968d6c0e4ab3b162bcbe7"
   license "MIT"
 
   depends_on "rust" => :build
